5cf103eec867db1684b3f243f4e635df6f119a39
Martin Snake.AAS Grossmann <snake.aas@gmail.com>
2013-08-20 09:49:58 +0200

    Added autocopy of ckeditor files and preset of ckeditor

--- snakeaas/NetteCKEditor/NetteCKEditor.php
+++ snakeaas/NetteCKEditor/NetteCKEditor.php
@@ -10,7 +10,6 @@ namespace snakeaas\NetteCKEditor;
 use Nette\Application\UI\Control;
 use Nette\Application\UI\Form;
 
-
 /**
  * Class NetteCKEditor
  * @package snakeaas\netteCKEditor
@@ -18,10 +17,12 @@ use Nette\Application\UI\Form;
 class NetteCKEditor extends Control {
 
 	protected $form;
+	protected $wwwDir;
 
 
 	public function __construct() {
-		$this->form = new Form();
+		$this->form   = new Form();
+		$this->wwwDir = null;
 	}
 
 
@@ -38,6 +39,9 @@ class NetteCKEditor extends Control {
 	 * @return Form
 	 */
 	public function createComponentCkeditor() {
+
+		$this->copyAssets();
+
 		$this->form->addTextArea('editor', null, 80, 10)
 			->setHtmlId('editor');
 
@@ -49,6 +53,13 @@ class NetteCKEditor extends Control {
 	}
 
 
+	protected function copyAssets() {
+		if ($this->wwwDir && !file_exists($this->wwwDir . '/ckeditor')) {
+			self::copy(__DIR__ . '/../../ckeditor', $this->wwwDir . '/ckeditor');
+		}
+	}
+
+
 	/**
 	 * @param Form $form
 	 */
@@ -56,8 +67,44 @@ class NetteCKEditor extends Control {
 
 	}
 
+
 	public function getForm() {
 		return $this->form;
 	}
 
-} 
+
+	/**
+	 * @return string|NULL
+	 */
+	public function getWwwDir() {
+		return $this->wwwDir;
+	}
+
+
+	/**
+	 * @param string $wwwDir
+	 */
+	public function setWwwDir($wwwDir) {
+		$this->wwwDir = $wwwDir;
+	}
+
+
+
+	static function copy($source, $dest, $overwrite = true) {
+		$dir = opendir($source);
+		@mkdir($dest);
+		while(false !== ($file = readdir($dir))) {
+			if (($file != '.') && ($file != '..')) {
+				if(is_dir($source . '/' . $file)) {
+					self::copy($source . '/' . $file, $dest . '/' . $file);
+
+				} else {
+					if($overwrite || !file_exists($dest . '/' . $file)) {
+						copy($source . '/' . $file, $dest . '/' . $file);
+					}
+				}
+			}
+		}
+		closedir($dir);
+	}
+}
